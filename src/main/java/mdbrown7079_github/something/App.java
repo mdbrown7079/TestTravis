7642687767cb595abc596990439030c300de62e5
Matt Brown <mdbrown7079@users.noreply.github.com>
2018-02-09 15:45:05 -0800

    switch to maven

---
+++ src/main/java/mdbrown7079_github/something/App.java
@@ -0,0 +1,13 @@
+package mdbrown7079_github.something;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
